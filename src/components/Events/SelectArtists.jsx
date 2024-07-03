281c44cbbff0593ec2d18d149be744f3727431f1
Sam Shacter <sshacter@gmail.com>
2024-07-03 16:00:00 -0500

    modified reducer for selection to be able to remove dj also

--- src/components/Events/SelectArtists.jsx
+++ src/components/Events/SelectArtists.jsx
@@ -1,31 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { Button, Card, CardContent, CardMedia, Typography, Grid, Stack } from "@mui/material";
 import Chip from "@mui/material/Chip";
 
 const SelectArtists = () => {
-    const history = useHistory()
-    const newEvent = useSelector(store => store.createReducer)
-    const djList = useSelector(store => store.djListReducer)
-    const suggestedDJs = useSelector(store => store.suggestionReducer)
-    const dispatch = useDispatch()
+    const history = useHistory();
+    const newEvent = useSelector((store) => store.createReducer);
+    const djList = useSelector((store) => store.djListReducer);
+    const suggestedDJs = useSelector((store) => store.suggestionReducer);
+    const dispatch = useDispatch();
+
+    const [selectedDJs, setSelectedDJs] = useState([]);
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_ALL_DJS' })
+        dispatch({ type: 'FETCH_ALL_DJS' });
         if (newEvent.genres && newEvent.genres.length > 0) {
-            dispatch({ type: 'FETCH_DJS_BY_GENRES', payload: newEvent.genres })
+            dispatch({ type: 'FETCH_DJS_BY_GENRES', payload: newEvent.genres });
         }
-    }, [dispatch, newEvent.genres])
+    }, [dispatch, newEvent.genres]);
 
     const addToEvent = (dj) => {
-        dispatch({ type: 'ADD_DJ_TO_EVENT', payload: dj });
-    }
+        if (isDJSelected(dj.dj_id)) {
+            setSelectedDJs((prevSelected) => prevSelected.filter((id) => id !== dj.dj_id));
+            dispatch({ type: 'REMOVE_DJ_FROM_EVENT', payload: dj });
+        } else {
+            setSelectedDJs((prevSelected) => [...prevSelected, dj.dj_id]);
+            dispatch({ type: 'ADD_DJ_TO_EVENT', payload: dj });
+        }
+    };
 
     const createEvent = () => {
         dispatch({ type: 'CREATE_EVENT', payload: newEvent });
         history.push('/events');
-    }
+    };
+
+    const isDJSelected = (dj_id) => selectedDJs.includes(dj_id);
 
     return (
         <div>
@@ -35,10 +45,10 @@ const SelectArtists = () => {
             <Typography variant="h5" gutterBottom>
                 Suggested DJs:
             </Typography>
-            <Grid container justifyContent="left" spacing={2}>
+            <Grid container justifyContent="center" spacing={2}>
                 {suggestedDJs.map((dj) => (
                     <Grid item xs={12} sm={6} md={3} lg={2} key={dj.dj_id}>
-                        <Card sx={{ backgroundColor: '#1b2961' }}>
+                        <Card sx={{ backgroundColor: isDJSelected(dj.dj_id) ? '#59b53f' : '#1b2961' }}>
                             <CardMedia
                                 component="img"
                                 height="400"
@@ -47,12 +57,14 @@ const SelectArtists = () => {
                                 sx={{ maxWidth: '100%', height: '250px', borderRadius: '0' }}
                             />
                             <CardContent>
-                                <Typography sx={{ color: 'white' }} variant="h5">{dj.dj_stage_name}</Typography>
+                                <Typography sx={{ color: 'white' }} variant="h5">
+                                    {dj.dj_stage_name}
+                                </Typography>
                                 <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                                     {dj.dj_genres.map((genre, index) => (
                                         <Chip
                                             key={index}
-                                            label={genre}
+                                            label={genre} 
                                             size="small"
                                             variant="outlined"
                                             sx={{ color: 'white', backgroundColor: '#1b2961', margin: '2px' }}
@@ -75,12 +87,14 @@ const SelectArtists = () => {
             <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
                 All DJs:
             </Typography>
-            <Grid container justifyContent="left" spacing={2}>
+            <Grid container justifyContent="center" spacing={2}>
                 {djList.map((dj) => (
                     <Grid item xs={12} sm={6} md={3} lg={2} key={dj.dj_id}>
-                        <Card sx={{ backgroundColor: '#1b2961' }}>
+                        <Card sx={{ backgroundColor: isDJSelected(dj.dj_id) ? '#59b53f' : '#1b2961' }}>
                             <CardContent>
-                                <Typography sx={{ color: 'white' }} variant="h4">{dj.dj_stage_name}</Typography>
+                                <Typography sx={{ color: 'white' }} variant="h4">
+                                    {dj.dj_stage_name}
+                                </Typography>
                                 <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                                     {dj.dj_genres.map((genre, index) => (
                                         <Chip
@@ -106,7 +120,10 @@ const SelectArtists = () => {
                 ))}
             </Grid>
             <Stack direction="row" justifyContent="center" spacing={2} style={{ marginTop: '30px' }}>
-                <Button variant="contained" size="large" onClick={createEvent}
+                <Button
+                    variant="contained"
+                    size="large"
+                    onClick={createEvent}
                     sx={{ backgroundColor: '#1b2961', border: '1px solid white' }}
                 >
                     Create Event
@@ -116,4 +133,4 @@ const SelectArtists = () => {
     );
 };
 
-export default SelectArtists
+export default SelectArtists;
